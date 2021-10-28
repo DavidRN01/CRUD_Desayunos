3d98cb2ea377c906145e39039948ceeeea8eefb9
DavidRamosNavas <DavidRamosNavas@CS-EST-PC-13145>
2021-10-28 09:16:28 +0200

    Diseño de la ventana

--- src/main/java/com/mycompany/cruddesayunos/Ventana.java
+++ src/main/java/com/mycompany/cruddesayunos/Ventana.java
@@ -27,21 +27,88 @@ public class Ventana extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        seleccionLeer = new javax.swing.JDialog();
+        btnLeerHoy = new javax.swing.JButton();
+        btnLeerTodo = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
+        info = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tabla = new javax.swing.JTable();
+        jPanel2 = new javax.swing.JPanel();
+        btnCarta = new javax.swing.JButton();
+        btnCrear = new javax.swing.JButton();
+        btnLeer = new javax.swing.JButton();
+        btnRecogido = new javax.swing.JButton();
+        btnBorrar = new javax.swing.JButton();
+
+        seleccionLeer.setResizable(false);
+        seleccionLeer.getContentPane().setLayout(new java.awt.GridLayout());
+
+        btnLeerHoy.setText("Leer solo las de hoy");
+        seleccionLeer.getContentPane().add(btnLeerHoy);
+
+        btnLeerTodo.setText("Leer todas la comandas");
+        seleccionLeer.getContentPane().add(btnLeerTodo);
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        jPanel1.setLayout(new java.awt.BorderLayout());
+
+        info.setText("Mostrando:");
+        jPanel1.add(info, java.awt.BorderLayout.PAGE_START);
+
+        tabla.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(tabla);
+
+        jPanel1.add(jScrollPane1, java.awt.BorderLayout.CENTER);
+
+        getContentPane().add(jPanel1, java.awt.BorderLayout.CENTER);
+
+        btnCarta.setText("Mostrar carta");
+        jPanel2.add(btnCarta);
+
+        btnCrear.setText("Crear comanda");
+        btnCrear.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCrearActionPerformed(evt);
+            }
+        });
+        jPanel2.add(btnCrear);
+
+        btnLeer.setText("Leer comandas");
+        btnLeer.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLeerActionPerformed(evt);
+            }
+        });
+        jPanel2.add(btnLeer);
+
+        btnRecogido.setText("Marcar como recogido");
+        jPanel2.add(btnRecogido);
+
+        btnBorrar.setText("Borrar comanda");
+        jPanel2.add(btnBorrar);
+
+        getContentPane().add(jPanel2, java.awt.BorderLayout.SOUTH);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnCrearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCrearActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnCrearActionPerformed
+
+    private void btnLeerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLeerActionPerformed
+        seleccionLeer.setSize(350, 88);
+        seleccionLeer.setVisible(true);
+    }//GEN-LAST:event_btnLeerActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +146,18 @@ public class Ventana extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnBorrar;
+    private javax.swing.JButton btnCarta;
+    private javax.swing.JButton btnCrear;
+    private javax.swing.JButton btnLeer;
+    private javax.swing.JButton btnLeerHoy;
+    private javax.swing.JButton btnLeerTodo;
+    private javax.swing.JButton btnRecogido;
+    private javax.swing.JLabel info;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JDialog seleccionLeer;
+    private javax.swing.JTable tabla;
     // End of variables declaration//GEN-END:variables
 }
