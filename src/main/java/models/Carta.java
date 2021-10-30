bcfa02d3c0f7ccb3fb6027638c9a8360820e7c93
DavidRN123 <davidramosnavas123@gmail.com>
2021-10-30 22:59:33 +0200

    Modelos

---
+++ src/main/java/models/Carta.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author david
+ */
+public class Carta {
+    
+    private String nombre;
+    private double precio;
+
+    public Carta() {
+    }
+
+    public Carta(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Carta{nombre=" + nombre + ", precio=" + precio + '}';
+    }
+    
+    
+    
+}
